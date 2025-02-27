83d02140de6da9d78b7aadc29926ac222d44cd02
gksdyd <gksdyd009@naver.com>
2025-02-27 12:01:07 +0900

    1. user_pet Init

---
+++ pet/src/test/java/com/user_pet/demo/PetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.user_pet.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
